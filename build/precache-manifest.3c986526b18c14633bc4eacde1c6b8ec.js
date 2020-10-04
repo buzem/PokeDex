71039bd21e88bb2a5873514a0220f61f0390935a
Berkay Özerbay <berkayozerbay@hotmail.com>
2020-10-04 16:31:27 +0300

    unnec elems deleted

---
+++ build/precache-manifest.3c986526b18c14633bc4eacde1c6b8ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11a03cf5f3c361094bc0535f18bfe02a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d7ce0d428d0b98d5074",
+    "url": "/static/css/2.c11e63b2.chunk.css"
+  },
+  {
+    "revision": "4d81801f806009fc96ca",
+    "url": "/static/css/main.6a5a269a.chunk.css"
+  },
+  {
+    "revision": "9d7ce0d428d0b98d5074",
+    "url": "/static/js/2.4666e99f.chunk.js"
+  },
+  {
+    "revision": "8bb757413c38ce564bf5d6ba97eb4669",
+    "url": "/static/js/2.4666e99f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d81801f806009fc96ca",
+    "url": "/static/js/main.ae6608d0.chunk.js"
+  },
+  {
+    "revision": "250103716dfc89e5abb8",
+    "url": "/static/js/runtime-main.9ff9079d.js"
+  },
+  {
+    "revision": "ffb664f4e05cb626bb8c6c68117385db",
+    "url": "/static/media/pattern3.ffb664f4.png"
+  }
+]);
